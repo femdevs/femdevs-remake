75cc6be61db53080d973e0a6955ca8c620c2f49a
Benjamin <benpai@thefemdevs.com>
2024-08-31 11:29:56 -0400

    fix language duplication glitch

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -1,7 +1,7 @@
 export default function NavLink({ href, text, extraAttributes }) {
     return (
         <a
-            className="select-none font-poppins text-lg transition-all hover:text-neutral-800 text-slate-600"
+            className="select-none font-poppins text-lg transition-all hover:text-neutral-800 text-slate-800"
             href={href}
             {...extraAttributes}
         >{text}</a>
