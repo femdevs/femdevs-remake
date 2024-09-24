4a6859e2107307765f89eed30cda76faf147645b
Benjamin <benpai@thefemdevs.com>
2024-09-24 11:17:47 -0400

    fix logo render

--- app/assets/images/icon/[color]/route.js
+++ app/assets/images/icon/[color]/route.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 export async function GET(request, { params }) {
     const { color } = params;
-    const file = fs.readFileSync(`${process.cwd()}/app/assets/media/logos/${color}.svg`);
+    const file = fs.readFileSync(`${process.cwd()}/assets/media/logos/${color}.svg`);
     return new NextResponse(file, {
         headers: {
             'Cache-Control': 'public, max-age 10800, max-stale 10800, stale-if-error 86400, no-transform, immutable',
